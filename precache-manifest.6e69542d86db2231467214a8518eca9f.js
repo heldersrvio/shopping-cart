6fa88d733ab579f7620f40ef27f0d2ba693f3c4d
Helder S�rvio <heldersrvio@gmail.com>
2020-07-13 20:29:08 -0300

    Updates

---
+++ precache-manifest.6e69542d86db2231467214a8518eca9f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf786ec3c4d3a501ebcdc3ece10ae0c6",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "d68d1b6fc09c080c6df3",
+    "url": "/Shopping-Cart/static/css/main.b71370af.chunk.css"
+  },
+  {
+    "revision": "4f3dd21e8820d7bf47da",
+    "url": "/Shopping-Cart/static/js/2.5550fc62.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Shopping-Cart/static/js/2.5550fc62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68d1b6fc09c080c6df3",
+    "url": "/Shopping-Cart/static/js/main.40173f84.chunk.js"
+  },
+  {
+    "revision": "dc4ddcf3b8b5b79ccf07",
+    "url": "/Shopping-Cart/static/js/runtime-main.ed685d3e.js"
+  },
+  {
+    "revision": "2e41cae52cd8ff3f02c554633deaf0b2",
+    "url": "/Shopping-Cart/static/media/DanmachiOratoria.2e41cae5.jpg"
+  },
+  {
+    "revision": "9a71c7c409af6208e064d54fed6e1555",
+    "url": "/Shopping-Cart/static/media/KotlinStandardLibrary.9a71c7c4.png"
+  },
+  {
+    "revision": "4ff7239668b921c39bfe925e3f9dcf21",
+    "url": "/Shopping-Cart/static/media/Lear.4ff72396.jpg"
+  },
+  {
+    "revision": "2da842d1bc708e42695696e5b5cc52d6",
+    "url": "/Shopping-Cart/static/media/NyahyssavEmiliLonneberga.2da842d1.jpg"
+  },
+  {
+    "revision": "43c391b89d542b56c63826646ad48946",
+    "url": "/Shopping-Cart/static/media/Python.43c391b8.jpg"
+  },
+  {
+    "revision": "7a2ae5194c832c32968811561ed35718",
+    "url": "/Shopping-Cart/static/media/TennouNoNihonshi.7a2ae519.jpg"
+  },
+  {
+    "revision": "9b4267c07c73a0c6099650d9fd3e9933",
+    "url": "/Shopping-Cart/static/media/ThePriorityOfTheOrangeTree.9b4267c0.jpg"
+  },
+  {
+    "revision": "cd8e8201e23d87c0d192e0484179a631",
+    "url": "/Shopping-Cart/static/media/iOS13.cd8e8201.jpg"
+  }
+]);
